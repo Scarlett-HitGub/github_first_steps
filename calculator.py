98305a5a4008ff22796e442716e856f772357f37
scarlett <eileen.scarlett.herrera@gmail.com>
2026-02-22 18:04:47 -0300

    Add calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+x = float(input("what's x?"))
+y = float(input("what's y?"))
+
+# int function converte de typed number,
+# into a anactual numbre for the function
+
+z = round(x + y)
+print(f"{z:,}")
--- lesson1.py
+++ lesson1.py
@@ -1,8 +1,16 @@
 # Ask user for their name
 name = input("what's your name? ")
 
+
+#Remove whitespace from str 
+#and capitalize user's name
 name = name.strip()
 name = name.title()
+
+#split user's name into first name and last name
+first, last = name.split(" ")
+
+
 # Say hello to user
 print("hello, ", end="")
 print(f'"{name}"')
