5d8c5897b8afef69ad67a910aa1c10b6ea369894
scarlett <eileen.scarlett.herrera@gmail.com>
2026-02-22 21:30:55 -0300

    update,if,elif,else

---
+++ compare.py
@@ -0,0 +1,21 @@
+# INTeger  = numeros enteros (1,2,3,-1,-2,-3)
+# FLOATing = numeros que representan decimales
+#print((1 + 2))
+#print((1 / 2))
+
+#print((int(1 / 2) + 1 / 2))  # int
+#print(int((1 / 2) + 1 / 2))  # int
+
+x = int(input("duh fuck is x?"))
+y = int(input("duh fuck is y?"))
+
+if x < y:
+    print(f"{x} is less than {y}")
+elif x > y:
+    print(f"{x} is more than {y}")
+elif x != y:
+    print(f"{x} is different than {y}")
+else:
+    print(f"{x} is equal to {y}")
+
+
